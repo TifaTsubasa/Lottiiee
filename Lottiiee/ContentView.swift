79f878c3850ebad6ce13d15ea469d742bd3fc74e
xiexufeng <xiexufeng@bytedance.com>
2025-02-21 16:02:01 +0800

    feat: init project

--- Lottiiee/ContentView.swift
+++ Lottiiee/ContentView.swift
@@ -6,16 +6,29 @@
 //
 
 import SwiftUI
+import UIKit
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let htmlFileName: String
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.isInspectable = true
+        return webView
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        if let url = Bundle.main.url(forResource: htmlFileName, withExtension: "html", subdirectory: "build") {
+            webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        WebView(htmlFileName: "index")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
