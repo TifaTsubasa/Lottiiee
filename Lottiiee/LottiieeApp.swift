ce6987fcc467de9d476c10ef7da2a1f223c3fc22
xiexufeng <xiexufeng@bytedance.com>
2025-02-21 12:00:06 +0800

    Initial Commit

---
+++ Lottiiee/LottiieeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottiieeApp.swift
+//  Lottiiee
+//
+//  Created by Yuri on 2025/2/21.
+//
+
+import SwiftUI
+
+@main
+struct LottiieeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LottiieeTests/LottiieeTests.swift
@@ -0,0 +1,16 @@
+//
+//  LottiieeTests.swift
+//  LottiieeTests
+//
+//  Created by Yuri on 2025/2/21.
+//
+
+import Testing
+
+struct LottiieeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
